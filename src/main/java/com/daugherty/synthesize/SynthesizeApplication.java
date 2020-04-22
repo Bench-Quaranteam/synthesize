715e876286d343eafe2e4e3dbf31692c6747e5c6
Joseph Dasuki <josephdasuki@MSP42JLD061022.local>
2020-04-22 14:23:54 -0500

    Initial spring boot app

---
+++ src/main/java/com/daugherty/synthesize/SynthesizeApplication.java
@@ -0,0 +1,13 @@
+package com.daugherty.synthesize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthesizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthesizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daugherty/synthesize/SynthesizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daugherty.synthesize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthesizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
