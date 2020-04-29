a0c932051ed8cbb770c8419afbfa23ffadd3af10
Lee Tran <leetran@MSP42LST061022.local>
2020-04-29 10:50:22 -0500

    fix tests

--- src/test/java/com/daugherty/synthesize/controller/ControllerTests.java
+++ src/test/java/com/daugherty/synthesize/controller/ControllerTests.java
@@ -15,6 +15,6 @@ public class ControllerTests {
 
     @Test
     public void testGetNumber() {
-        assertEquals(-2, controller.getNumber());
+        assertEquals(123, controller.getNumber());
     }
 }
